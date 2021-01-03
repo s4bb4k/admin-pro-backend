946b7f31d4d1743bfcd9ad6d52c72b304abf6c8d
s4bb4k <ch.morales@outlook.com>
2021-01-03 16:48:24 -0500

    se sube path * en rutas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
+
 var cors = require('cors');
 
 const { dbConnection } = require('./database/config');
@@ -27,6 +29,9 @@ app.use('/api/medicos', require('./routes/medicos-routes'));
 app.use('/api/todo', require('./routes/busquedas-routes'));
 app.use('/api/uploads', require('./routes/uploads-routes'));
 
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html') );
+})
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT);
